5f0435f23c213722ccd72c4e461f2e6c806755d8
atticup <admin@pfuner.xyz>
2025-12-13 18:18:09 +0300

    funny

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const versionHandler = require("./api/version");
+const versionHandler = require("./version");
+const lyricsHandler = require("./lyrics");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("/version", versionHandler);
+app.get("/", lyricsHandler.page);
+app.post("/lyrics", lyricsHandler.search);
 
 module.exports = app;
